b833b37f2e09a4630e0f57d83c5ad52ceec309af
Vlad <vlad.ios.develop@gmail.com>
2022-04-05 09:17:57 +0300

    Create project

---
+++ Spotify/Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Vlad Panchenko on 05.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
